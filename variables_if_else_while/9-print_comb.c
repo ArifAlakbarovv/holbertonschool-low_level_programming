768567429624741ccfdcdbd222ea164711537771
ArifAlakbarovv <arif12342031@gmail.com>
2024-02-19 06:22:24 -0800

    up

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Arif
+ */
+#include <stdio.h>
+/**
+ * main - Write a program that prints all possible combinations of
+ * single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
