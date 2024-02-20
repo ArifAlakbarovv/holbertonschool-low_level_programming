7b771ea89ddfa3c92af7f246f7f25b7dfdffc12a
ArifAlakbarovv <arif12342031@gmail.com>
2024-02-20 06:11:48 -0800

    up

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet(void);
+int _putchar(char c);
+
+#endif /*MAIN_H*/
+
+/*1-alphabet.c*/
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char letter='a';
+
+	while (letter<=z)
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
