8a268fc8d76cf86aa5e7e5d1f6ddd57179339936
ArifAlakbarovv <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-02-27 21:28:29 +0400

    Update 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
+ * _strcpy - Copies a string pointed to by @src to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
  *
@@ -10,13 +9,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+    int index = 0;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+    while (src[index] != '\0')
+    {
+        dest[index] = src[index];
+        index++;
+    }
 
-	return (dest);
+    dest[index] = '\0';  /* Ensure the destination string is null-terminated */
+
+    return dest;
 }
+
