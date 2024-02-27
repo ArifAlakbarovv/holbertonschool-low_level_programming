2ba008bb7df2e9a08bdb0087e4ff19793d56e07f
ArifAlakbarovv <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-02-27 22:11:40 +0400

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,20 @@
 #include <string.h>
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- *
- * Return: A pointer to the destination string @dest.
- */
+ * my code
+ *//
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+    int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+    while (dest[index++])
+        dest_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+    for (index = 0; src[index]; index++)
+        dest[dest_len++] = src[index];
 
-	return (dest);
+    dest[dest_len] = '\0';  // Ensure the destination string is null-terminated
+
+    return dest;
 }
+
